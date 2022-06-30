2d14f8c48eed6c5ff4d8e55d336ab04e66f12ac7
Leonardo13344 <lrsuniga@uce.edu.ec>
2022-06-29 21:05:57 -0500

    tarea 9

--- js/estudiante.js
+++ js/estudiante.js
@@ -7,15 +7,27 @@ const lista = [
     { nombre: 'Carlos', apellido: 'Duarte', edad: 19 }
 ]
 
+const profes = [
+    {nombre: 'Jose', apellido: 'Garcia', edad:40, genero:'M', sueldo: 1000},
+    {nombre: 'Maria', apellido: 'Vanegas', edad:35, genero:'F', sueldo: 970},
+    {nombre: 'Nicolas', apellido: 'Sanchez', edad:45, genero:'M', sueldo: 1456},
+    {nombre: 'Genesis', apellido: 'Aguirre', edad:31, genero:'F', sueldo: 770},
+    {nombre: 'Alexa', apellido: 'Montero', edad:60, genero:'F', sueldo: 2000}
+]
+
 const app = Vue.createApp({
     data() {
         return {
             lista,
+            profes
         }
     },
     methods: {
         agregarEstudiante() {
             this.lista.unshift(  { nombre: 'Anthonny', apellido: 'Montero', edad: 19 })
+        },
+        agregarProfes(){
+            this.profes.unshift({ nombre: 'Jorge', apellido: 'Mateus', edad:52, genero:'M', sueldo: 1450})
         }
     }
 })
